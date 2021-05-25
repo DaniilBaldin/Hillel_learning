0201754d82cd42be855fbb259c3348fb71f0e5b9
ildan <daniil_baldin@ukr.net>
2021-05-25 18:11:17 +0300

    First commit

---
+++ HW4_task1.py
@@ -0,0 +1,9 @@
+my_value_int = int(input('Введите любое значение:'))
+print(my_value_int)
+my_value_str = str(my_value_int)
+print(my_value_str)
+my_value_float = float(my_value_int)
+print(my_value_float)
+
+
+
