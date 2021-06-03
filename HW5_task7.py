e47b041700dac70659014bfffbd53cc2123d6c48
ildan <daniil_baldin@ukr.net>
2021-06-03 15:04:08 +0300

    First commit

---
+++ HW5_task7.py
@@ -0,0 +1,10 @@
+x = int(input("Введите координаты x="))
+y = int(input("Введите координаты y="))
+if x > 0 and y > 0:
+    print('I')
+elif x < 0 < y:
+    print('II')
+elif x < 0 and y < 0:
+    print('III')
+elif x > 0 > y:
+    print('IV')
